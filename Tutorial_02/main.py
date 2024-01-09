6bc5b6f9b52096391802c46fdf1a044c552671b6
namkidong <rlehd201@naver.com>
2024-01-09 13:48:59 +0900

    modify : main.py

--- Tutorial_02/main.py
+++ Tutorial_02/main.py
@@ -7,7 +7,7 @@ from typing import Optional
 app = FastAPI()
 
 fake_items_db = [{"itme_name" : "Foo"}, {"itme_name" : "Bar"}, {"itme_name" : "Baz"}]
-@app.get("/items")
+@app.get("/items/list")
 async def list_items(skip: int = 0, limit: int = 10):
     return fake_items_db[skip: skip+limit]
 
@@ -19,3 +19,50 @@ async def get_item(item_id: str, q: Optional[str] = None, short : bool = False):
     if not short:
         item.update({"description" : "This is the item"})
     return item
+
+from pydantic import BaseModel
+# BaseModel: 상속받은 모델은 클래스 변수로 데이터의 필드와 해당 필드의 유효성 검사 규칙을 정의 가능 
+
+class Item(BaseModel):
+    name : str
+    description : Optional[str] = None
+    price : float
+    tax : Optional[float] = None
+    #tax : float | None = None # python 3.9 version이라 오류 발생
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax" : price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id : int, item : Item, q : Optional[str] = None):
+    result = {"item_id" : item_id, **item.dict()}
+    if q:
+        result.update({"q" : q})
+    return result
+
+from fastapi import Query
+
+# @app.get("/items")
+# async def read_items(q: Optional[list[str]] = Query(None, max_length=10)):
+#     results = {"items" : [{"item_id" : "Foo"}, {"item_id" : "Bar"}]}
+#     if q:
+#         results.update({"q" : q})
+#     return results
+
+@app.get("/items")
+async def read_items(q: Optional[list[str]] = Query(None)):
+    results = {"items" : [{"item_id" : "Foo"}, {"item_id" : "Bar"}]}
+    if q:
+        results.update({"q" : q})
+    return results
+
+@app.get("/items_hidden")
+async def hidden_query_route(hidden_query: Optional[str] = Query(None, include_in_schema=False)):
+    if hidden_query:
+        return {"hidden_query" : hidden_query}
+    return {"hidden_query" : "Not found"}
